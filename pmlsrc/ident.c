b67d09f399f0519e5463dc063f2f997f1ddb3ad5
Thorsten Otto <admin@tho-otto.de>
2019-04-09 15:48:08 +0200

    Update the Ident string

--- pmlsrc/ident.c
+++ pmlsrc/ident.c
@@ -1,13 +1,15 @@
 #include "PatchLev.h"
-char __Ident_pml[] = "$PatchLevel: atariST/TT ("
-#if defined __mcoldfire__
-"cfv4e"
-#elif !defined (__M68881__) && !defined (sfp004)
-"68000"
-#else
-"68020-60"
+char __Ident_math[] = "$Version: PML "
+#if defined(__mc68020__) || defined(__mc68030__) || defined(__mc68040__) || defined(__mc68060__)
+"-m68020-60 "
+#endif
+#ifdef __mcoldfire__
+"-mcpu=5475 "
+#endif
+#ifdef __M68881__
+"-m68881 "
 #endif
 #ifdef __MSHORT__
-"/mshort"
+"-mshort "
 #endif
-") PML: " PatchLevel " $";
+PatchLevel" $";
